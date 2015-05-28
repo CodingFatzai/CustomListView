e17fd8e65e36b4bc50d6aca216ea098acdca73df
jiacheng_ubuntu <s124202468@gmail.com>
2015-05-28 23:35:09 +0800

    2015/05/28 CustomListview

---
+++ app/src/main/java/com/example/jiacheng/customlistview/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.jiacheng.customlistview;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+
+
+
+
+public class MainActivity extends ListActivity {
+    String[] itemname = {
+            "Safari",
+            "Camera",
+            "Global",
+            "FireFox",
+            "UC Browser",
+            "Android Folder",
+            "VLC Player",
+            "Cold War"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.setListAdapter(new ArrayAdapter<String>(
+                this, R.layout.fragment0_listitem,
+                R.id.Itemname, itemname));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
